9df485d97518f04e21705423360c3bf1d705adc6
Omkar <omkar.achrekar007@gmail.com>
2020-12-26 21:06:07 +0530

    Enable Django admin for user profile model

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from profiles_api import models
+
+# This tells django admin to register our user profile model with the admin site
+# so it makes accessible through admin interface
+admin.site.register(models.UserProfile)
